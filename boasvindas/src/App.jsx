3258eb6b482d30c4768b2842f3ecfb62bd639d27
marinrkyy <marina.caetano@etec.sp.gov.br>
2025-04-08 14:59:03 -0300

    0804

---
+++ boasvindas/src/App.jsx
@@ -0,0 +1,21 @@
+import BoasVindas from "./exemplo1"
+
+function App() {
+ 
+
+  return (
+    <div>
+<h1>Boas Vindas ao REACT com JAVASCRIPT</h1>
+
+<p>Este é o App.jsx </p>
+
+<BoasVindas nome="Billie Eilish" />
+<BoasVindas nome="Oscar Piastri" />
+<BoasVindas nome="Hailee Stenfield" />
+<BoasVindas nome="Gracie Abrams" />
+<BoasVindas nome="Lando Norris" />
+    </div>
+  )
+}
+
+export default App
